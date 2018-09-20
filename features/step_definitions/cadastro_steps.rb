c54bceef365365f7f23786a0faa6ef5434c55794
Luiz Macedo <luizmacedo0788@hotmail.com>
2018-09-19 23:57:13 -0300

    todos os testes funcionando

--- features/step_definitions/cadastro_steps.rb
+++ features/step_definitions/cadastro_steps.rb
@@ -1,6 +1,8 @@
 require 'faker'
 
 Dado('que estou na página de cadastro') do
+  @nav = Nav.new
+
   visit '/api/reset' # Gatenho
   visit '/cadastre-se'
 end
@@ -8,7 +10,7 @@ end
 Quando('faço o meu cadastro com') do |table|
   @usuario = table.rows_hash
 
-  fill_in 'full-name', with: @usuario[:nome]
+  fill_in 'fullName', with: @usuario[:nome]
   fill_in 'email', with: @usuario[:email]
   fill_in 'password', with: @usuario[:senha]
 
@@ -16,6 +18,6 @@ Quando('faço o meu cadastro com') do |table|
 end
 
 Então('devo ver a mensagem {string}') do |mensagem_alerta|
-  alerta = find('.s-alert-box')
+  alerta = find('.alert')
   expect(alerta).to have_content mensagem_alerta
 end
